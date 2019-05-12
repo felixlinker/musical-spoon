ae5afbf9afc4f416c33dc548a3c50d0852025558
christopher-bioinf <50520076+christopher-bioinf@users.noreply.github.com>
2019-05-12 21:25:45 +0200

    Add files via upload
    Updated File.
    Liefert jetzt ein korrektes zufallsgeneriertes Graph-Objekt innerhalb frei wählbarer Grenzparameter. Aktuell nur ungerichtete Graphen. 
    Optimierung...folgt.

--- random_graph.py
+++ random_graph.py
@@ -6,14 +6,72 @@ Created on Sat May 11 15:17:23 2019
 """
 
 #TODO: -Bei Block2 wird ein ; zu viel eingetragen, wird morgen in Ordnung gebracht
-#      -Generierung einer Instanz der Graph-Klasse als Rückgabewert, 
-#      -Schleifen prüfen
+        
 #      -Implementierung gerichteter Graphen
 
 import numpy as np
+from Vertex import Vertex
+from Edge import Edge
+from Graph import Graph
 
 
-def random_graph(lower_node_limit, upper_node_limit, nodes_labeled, edges_labeled, directed):
+def random_graph(lower_node_limit, upper_node_limit, nodes_labeled, edges_labeled):
+    
+    n_nodes = 0
+    n_edges = 0
+    vertex_list = []
+    edge_list =[]
+    
+    #Number of nodes 
+    n_nodes = np.random.randint(lower_node_limit, upper_node_limit +1)
+    
+    #Number of edges
+    edges_limit = (n_nodes*(n_nodes -1))/2
+    n_edges = np.random.randint(1, edges_limit +1)
+    
+    #Generate a set of vertices - at the moment without label
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    for i in range(0, n_nodes):
+        
+        if i < len(alphabet):
+            c = alphabet[i]
+            node = Vertex(c)
+            vertex_list.append(node)
+        else:
+            node_name_over_z = "z"
+            node_name_over_z += str(i)
+            node = Vertex(node_name_over_z)
+            vertex_list.append(node)
+    
+    #Generate a set of edges
+    edge_matrix = np.zeros((n_nodes+1, n_nodes+1))
+    
+    for i in range(0, n_nodes):
+        edge_matrix[i][0] = i
+        edge_matrix[0][i] = i
+        
+   # while n_edges > 0:
+    for i in range(1, n_nodes):
+                
+            if i+1 < n_nodes:
+                x = np.random.randint(i+1, n_nodes)
+                if edge_matrix[x][i] == 0 and n_edges > 0:
+                    e = Edge(vertex_list[i], vertex_list[x])
+                    edge_matrix[i][x] = 1
+                    edge_matrix[x][i] = 1
+                    print(edge_matrix)
+                    edge_list.append(e)
+                    n_edges -= 1
+
+
+    #Create the Graph
+    g = Graph(vertex_list, edge_list)
+    return g
+            
+           
+
+def write_random_graph(lower_node_limit, upper_node_limit, nodes_labeled, edges_labeled, directed):
     
     targetfile = open("randomgraph.graph", "a")
     block1 = ""
@@ -28,7 +86,6 @@ def random_graph(lower_node_limit, upper_node_limit, nodes_labeled, edges_labele
     #Decide on a number of edges 
     edges_limit = (nodes*(nodes -1))/2
     edges = np.random.randint(1, edges_limit +1)
-    print(edges)
     block1 += "#edges;"
     block1 += str(edges)
 
@@ -66,7 +123,6 @@ def random_graph(lower_node_limit, upper_node_limit, nodes_labeled, edges_labele
                     if x > 0:
                         edge_list[i][b] = b
                         edge_list[b][i] = i
-                        print(edge_list)
                         edges -= 1
                     else:
                         continue
@@ -103,4 +159,5 @@ def random_graph(lower_node_limit, upper_node_limit, nodes_labeled, edges_labele
     targetfile.write(block1)
     targetfile.write(block2)
     targetfile.write(block3)    
+    targetfile.close()
 
