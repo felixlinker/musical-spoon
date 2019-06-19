b1e53ced5faba6c4685bbd70f678e354b8859bd1
roman-at-github <roman.matern@yahoo.de>
2019-06-19 19:34:24 +0200

    Reverse_parser Test

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from modular_product import modular_product
 from parser import parse
 from show_graph import show_graph
+from random_graph import *
 
 
 def main():
-    a = parse('Graphen/modularTestA.graph')
-    b = parse('Graphen/modularTestB.graph')
 
-    mod_graph = modular_product(a, b)
-
-    # prints edgelist of modular product
-    for edge in mod_graph.edges:
-        print(edge.vertex_a.name, '---', edge.vertex_b.name)
-
-    show_graph(mod_graph)
+    g = random_chess_graph(10,20,0.05)
+    reverse_parser(g)
 
 
 
