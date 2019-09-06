24cb366e6cf122e8810eea7c3332cd3a0778b3ca
roman-at-github <roman.matern@yahoo.de>
2019-09-06 12:10:58 +0200

    Tests

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from random_graph import *
 from bronk_pivot import *
 
 
+
 def main():
 
     g1 = parse('./Graphen/test.graph')
     g2 = parse('./Graphen/test.graph')
-    find_mcis(g1, g2)
-    #show_graph(g)
+    show_graph(g1)
+    g = find_mcis(g1, g2)
+    show_graph(g)
 
 
 
