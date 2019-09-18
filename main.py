f5b5b13e972ef3ad24be254ed2d02e6eedc95a49
roman-at-github <roman.matern@yahoo.de>
2019-09-18 20:16:00 +0200

    Test

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from modular_product import modular_product
 from parser2 import *
 from show_graph import show_graph
 from random_graph import *
@@ -8,16 +7,16 @@ from Cordella_max import Cordella
 
 def main():
 
-    g1 = parse("Graphen\graph6.graph")
-    g = parse("Graphen\graph5.graph")
-    # g1 = random_chess_graph(50, 50, 0)
-    # g = random_chess_graph(20, 20, 0)
-    
-    #reverse_parser(g)
-    #find_cliques(g)
-    # show_graph(g1)
-    # show_graph(g)
-    Cordella(g, g1)
+    g1 = parse("./Graphen/scriptG1.graph")
+    g2 = parse("./Graphen/scriptG2.graph")
+    # show_two_graphs(g1, g2)
+
+    anker = ['10;4']
+    mcis = find_ankered_mcis(g1, g2, anker)
+    show_graph_comparable(g1, g2, mcis)
+
+
+
 
 
 
